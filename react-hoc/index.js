3bded9ceceb5f1557567727bd7f251cda9e38eac
Alexandru Leca <alexandru.leca89@gmail.com>
2022-11-15 11:46:50 +0200

    wip hoc

--- react-hoc/index.js
+++ react-hoc/index.js
@@ -1,19 +1,26 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "https://esm.sh/react@18";
+import ReactDOM from "https://esm.sh/react-dom@18";
 
-const withLoadingFeedback = (Component) => (props) => {
-    if (props.isLoading) return <div>Loading data.</div>;
+const withLoadingFeedback = (feedback) => (Component) => (props) => {
+    if (props.isLoading) return <div>{feedback}</div>;
     return <Component {...props} />;
 };
 
-const withNoDataFeedback = (Component) => (props) => {
-    if (!props.data) return <div>No data loaded yet.</div>;
+const withNoDataFeedback = (feedback) => (Component) => (props) => {
+    if (!props.data) return <div>{feedback}</div>;
     return <Component {...props} />;
 };
 
-const withDataEmptyFeedback = (Component) => (props) => {
-    if (!props.data.length) return <div>Data is empty.</div>;
+const withDataEmptyFeedback = (feedback) => (Component) => (props) => {
+    if (!props.data.length) return <div>{feedback}</div>;
     return <Component {...props} />;
 };
+
+const compose = (...fns) =>
+    fns.reduceRight((prevFn, nextFn) =>
+            (...args) => nextFn(prevFn(...args)),
+        value => value
+    );
 
 const TodoItem = ({key, item}) => {
     return (
@@ -21,18 +28,34 @@ const TodoItem = ({key, item}) => {
     );
 };
 
-const BaseTodoList = ({ data }) => {
+const BaseTodoList = ({data}) => {
     return (
         <ul>
             {data.map((item) => (
-                <TodoItem key={item.id} item={item} />
+                <TodoItem key={item.id} item={item}/>
             ))}
         </ul>
     );
 };
 
-const TodoList = withLoadingFeedback(
-    withNoDataFeedback(
-        withDataEmptyFeedback(BaseTodoList)
-    )
-);
+const TodoList = compose(
+    withLoadingFeedback('Loading Todos.'),
+    withNoDataFeedback('No Todos loaded yet.'),
+    withDataEmptyFeedback('Todos are empty.')
+)(BaseTodoList);
+
+const App = () => {
+    const data = [
+        "TO DO #1",
+        "TO DO #2",
+        "TO DO #3"
+    ];
+    return(
+        <div className="box">
+            <TodoList data={data} isLoading />
+        </div>
+    );
+}
+
+ReactDOM.render(<App />,
+    document.getElementById("root"))
