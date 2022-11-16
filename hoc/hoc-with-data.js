bc7b12749e6ccb97a06d6d7d0ad0aa1e0f3ea6af
Alexandru Leca <alexandru.leca89@gmail.com>
2022-11-16 10:59:55 +0200

    some refactoring

--- hoc/hoc-with-data.js
+++ hoc/hoc-with-data.js
@@ -4,26 +4,30 @@ import ReactDOM from "https://esm.sh/react-dom@18";
 const withFetchData = (Component, url) => (props) => {
     const [data, setData] = useState({})
 
-    const fetchData = () => {
-        setData({success: true, url})
+    const fetchData = async () => {
+        fetch(`https://reqres.in/api${url}`, {
+            method: 'GET'
+        })
+            .then((response) => response.json())
+            .then((json) => setData(json))
     }
 
-    return (
-        <Component data={data} fetchData={fetchData} {...props} />
-    );
-}
-
-const BaseComponent = ({data, fetchData}) => {
     useEffect(() => {
         fetchData()
     }, []);
-    console.log(data);
+
     return (
-        <div>{JSON.stringify(data)}</div>
+        <Component data={data} {...props} />
+    );
+}
+
+const BaseComponent = ({data}) => {
+    return (
+        <pre>{JSON.stringify(data)}</pre>
     )
 }
 
-const ComponentWithFetch = withFetchData(BaseComponent, 'http://example.com')
+const ComponentWithFetch = withFetchData(BaseComponent, '/users/2')
 
 const App = () => {
 
